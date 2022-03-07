891d99dee43b9b730dc305879fa6c81c46caf0a4
holly <emperor.kurt@gmail.com>
2022-03-08 01:30:08 +0900

    add powershell autoload script

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,14 @@
+$ENV:PATH += ";$ENV:USERPROFILE\bin"
+
+Set-Alias vi nvim
+
+$myvimrc = "$ENV:LOCALAPPDATA\nvim\init.vim"
+
+Function uptime() {
+  [DateTime]::Now -  [Management.ManagementDateTimeConverter]::ToDateTime((Get-WmiObject Win32_OperatingSystem).LastBootUpTime) |
+  Select-Object Days, Hours, Seconds, Milliseconds| Format-Table -AutoSize
+}
+
+Function home() {
+  Set-Location $ENV:USERPROFILE
+}
