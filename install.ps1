5b2dfa0e6c15c3233980bc6a3e4660628240e75d
holly <emperor.kurt@gmail.com>
2022-03-11 10:43:30 +0900

    update installer. display elapsed seconds

--- install.ps1
+++ install.ps1
@@ -26,10 +26,11 @@ $NVIM_TEMPLATE_FILES = @("python.txt")
 $MY_SCRIPTS          = @("x86_64-w64-mingw32-gcc.ps1", "scoop_update.ps1")
 $SHORTCUTS           = @("$ENV:USERPROFILE\scoop\apps\neovim\current\bin\nvim-qt.exe")
 
+$START_DATE = Get-Date
 
-Write-Output "=== start ==="
+Write-Output "===== start windows work prepation ====="
 
-Write-Output "making directories"
+Write-Output ">> making directories"
 $DIRS | ForEach-Object {
     if (Test-Path $PSItem) {
         Write-Output "$PSItem is exists. skip"
@@ -39,7 +40,7 @@ $DIRS | ForEach-Object {
 }
 
 Write-Output ""
-Write-Output "install scoop"
+Write-Output ">> install scoop"
 if (Test-Path "$ENV:USERPROFILE\scoop") {
     Write-Output "scoop already installed. skip"
 } else {
@@ -50,26 +51,26 @@ if (Test-Path "$ENV:USERPROFILE\scoop") {
 
 
 Write-Output ""
-Write-Output "download powershell start script"
+Write-Output ">> download powershell start script"
 $save_path = "$ENV:USERPROFILE\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
 Invoke-WebRequest -Uri $PS_START_SCRIPT -OutFile $save_path
 Write-Output "save as $save_path"
 
 Write-Output ""
-Write-Output "download nvim init.vim"
+Write-Output ">> download nvim init.vim"
 $save_path = "$ENV:LOCALAPPDATA\nvim\init.vim"
 $url       = "$REPO_URL/nvim/init.vim"
 Invoke-WebRequest -Uri $url -OutFile $save_path
 Write-Output "save as $save_path"
 
 Write-Output ""
-Write-Output "download vim-plug"
+Write-Output ">> download vim-plug"
 $save_path = "$ENV:LOCALAPPDATA\nvim-data\site\autoload\plug.vim"
 Invoke-WebRequest -Uri $PLUG_VIM -OutFile $save_path
 Write-Output "save as $save_path"
 
 Write-Output ""
-Write-Output "download vim template"
+Write-Output ">> download vim template"
 $NVIM_TEMPLATE_FILES | ForEach-Object {
 
     $save_path = "$ENV:LOCALAPPDATA\nvim\template\$PSItem"
@@ -81,7 +82,7 @@ $NVIM_TEMPLATE_FILES | ForEach-Object {
 }
 
 Write-Output ""
-Write-Output "download my scripts"
+Write-Output ">> download my scripts"
 $MY_SCRIPTS | ForEach-Object {
 
     $save_path = "$ENV:USERPROFILE\bin\$PSItem"
@@ -93,18 +94,28 @@ $MY_SCRIPTS | ForEach-Object {
 }
 
 Write-Output ""
-Write-Output "install scoop apps"
+Write-Output ">> install scoop apps"
 Start-Process -FilePath powershell -ArgumentList $ENV:USERPROFILE\bin\scoop_update.ps1 -Wait
 
 Write-Output ""
-Write-Output "create desktop shortcut"
+Write-Output ">> create desktop shortcut"
 $SHORTCUTS | ForEach-Object {
+
+	$path = "$ENV:USERPROFILE\Desktop\" + [System.IO.Path]::GetFileName($PSItem) + ".lnk"
+
     $WSShell = New-Object -ComObject WScript.Shell
-    $ShortCut = $WSShell.CreateShortcut("$ENV:USERPROFILE\Desktop\" + [System.IO.Path]::GetFileName($PSItem) + ".lnk")
+    $ShortCut = $WSShell.CreateShortcut($path)
     $ShortCut.TargetPath   = $PSItem
     $ShortCut.IconLocation = $PSItem
     $ShortCut.Save()
+	
+    Write-Output "create shortcut as $path"
 }
 
+
+$END_DATE = Get-Date
+$elapced = ($END_DATE - $START_DATE).TotalSeconds 
+$last_message = "finished. (" + $elapced + "sec)"
+
 Write-Output ""
-Write-Output "finishd."
+Write-Output $last_message
