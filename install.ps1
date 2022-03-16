671143e7ee9a200754f33e6e9d3c501211100ba0
holly <emperor.kurt@gmail.com>
2022-03-17 02:31:46 +0900

    install.pl major change

--- install.ps1
+++ install.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = "Stop"
 
-$REPO_URL        = "https://raw.githubusercontent.com/holly/windows-work-preparation/main"
-$SCOOP_LIST      = "$REPO_URL/scoop.txt"
-$PS_START_SCRIPT = "$REPO_URL/WindowsPowerShell/Microsoft.PowerShell_profile.ps1"
-$SCOOP_INSTALLER = "https://get.scoop.sh"
-$PLUG_VIM        = "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+$REPO_URL            = "https://github.com/holly/windows-work-preparation.git"
+$REPO_RAW_URL        = "https://raw.githubusercontent.com/holly/windows-work-preparation/main"
+$SCOOP_LIST_URL      = "$REPO_RAW_URL/scoop.txt"
+$SCOOP_INSTALLER_URL = "https://get.scoop.sh"
+$PLUG_VIM_URL        = "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 
 $DIRS = @(
     "$ENV:USERPROFILE\Documents\WindowsPowerShell",
@@ -19,18 +19,20 @@ $DIRS = @(
     "$ENV:LOCALAPPDATA\nvim\pack\vack\after",
     "$ENV:LOCALAPPDATA\nvim\pack\vack\opt",
     "$ENV:LOCALAPPDATA\nvim\pack\vack\start",
-    "$ENV:USERPROFILE\bin"
+    "$ENV:USERPROFILE\bin",
+    "$ENV:APPDATA\pandoc",
+    "$ENV:APPDATA\pandoc\templates"
+
     )
 
-$NVIM_TEMPLATE_FILES = @("python.txt")
-$MY_SCRIPTS          = @("x86_64-w64-mingw32-gcc.ps1", "scoop_update.ps1")
-$SHORTCUTS           = @("$ENV:USERPROFILE\scoop\apps\neovim\current\bin\nvim-qt.exe")
 
-$START_DATE = Get-Date
+$SHORTCUTS  = @("$ENV:USERPROFILE\scoop\apps\neovim\current\bin\nvim-qt.exe")
 
-Write-Output "===== start windows work prepation ====="
+$StartDate = Get-Date
 
-Write-Output ">> making directories"
+Write-Output "===== Start windows work prepation ====="
+
+Write-Output ">> Make directories"
 $DIRS | ForEach-Object {
     if (Test-Path $PSItem) {
         Write-Output "$PSItem is exists. skip"
@@ -39,80 +41,113 @@ $DIRS | ForEach-Object {
     }
 }
 
+
 Write-Output ""
-Write-Output ">> install scoop"
-if (Test-Path "$ENV:USERPROFILE\scoop") {
-    Write-Output "scoop already installed. skip"
-} else {
-    # https://scoop.sh/
-    Invoke-Expression (New-Object System.Net.WebClient).DownloadString($SCOOP_INSTALLER)
+Write-Output ">> Install scoop"
+try {
+  # scoopのインストール確認
+  Get-Command scoop -ErrorAction Stop
+} 
+catch [Exception] {
+  # Scoopのインストール
+  Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+  Invoke-Expression (New-Object System.Net.WebClient).DownloadString($SCOOP_INSTALLER_URL)
+}
+
+
+Write-Output ""
+Write-Output ">> Install scoop apps"
+$Res = Invoke-WebRequest $SCOOP_LIST_URL
+$Body = $Res.Content
+$Body.Split("`n") | ForEach-Object {
+    $app = $PSItem.trim()
+    if ($app.Length -ne 0) {
+        scoop install $app
+    }
+}
+
+
+Write-Output ""
+Write-Output ">> Download windows-work-preparation repository"
+$LocalRepo = "$ENV:TEMP\windows-work-preparation"
+Push-Location "$ENV:TEMP"
+if (Test-Path $LocalRepo) {
+  Remove-Item -Path $LocalRepo -Force -Recurse
+}
+echo "git clone $REPO_URL"
+Start-Process -FilePath git -ArgumentList "clone $REPO_URL" -Wait
+Pop-Location
+
+
+Write-Output ""
+Write-Output ">> Set powershell start script"
+$Src = "$LocalRepo\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
+$Dest = $PROFILE
+Copy-Item -Path $Src -Destination $Dest
+Write-Output "Save as $Dest"
+
+
+Write-Output ""
+Write-Output ">> Save nvim init.vim"
+$Src  = "$LocalRepo\nvim\init.vim"
+$Dest = "$ENV:LOCALAPPDATA\nvim\init.vim"
+Copy-Item -Path $Src -Destination $Dest
+Write-Output "Save as $Dest"
+
+Write-Output ""
+Write-Output ">> Download vim-plug"
+$Dest = "$ENV:LOCALAPPDATA\nvim-data\site\autoload\plug.vim"
+Invoke-WebRequest -Uri $PLUG_VIM_URL -OutFile $Dest
+Write-Output "Save as $Dest"
+
+Write-Output ""
+Write-Output ">> Save vim template"
+Get-ChildItem -Path "$LocalRepo\nvim\template" -File | ForEach-Object {
+  $Src  = $PSItem.FullName
+  $Dest = "$ENV:LOCALAPPDATA\nvim\template\" + $PSItem.Name
+  Copy-Item -Path $Src -Destination $Dest
+  Write-Output "Save as $Dest"
+}
+
+
+Write-Output ""
+Write-Output ">> Save my scripts"
+Get-ChildItem -Path "$LocalRepo\bin" -File | ForEach-Object {
+  $Src  = $PSItem.FullName
+  $Dest = "$ENV:USERPROFILE\bin\" + $PSItem.Name
+  Copy-Item -Path $Src -Destination $Dest
+  Write-Output "Save as $Dest"
+}
+
+
+Write-Output ""
+Write-Output ">> Set pandoc templates"
+Get-ChildItem -Path "$LocalRepo\pandoc\templates" -File | ForEach-Object {
+  $Src  = $PSItem.FullName
+  $Dest = "$ENV:APPDATA\pandoc\templates\" + $PSItem.Name
+  Copy-Item -Path $Src -Destination $Dest
+  Write-Output "Save as $Dest"
 }
 
 
 
 Write-Output ""
-Write-Output ">> download powershell start script"
-$save_path = "$ENV:USERPROFILE\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
-Invoke-WebRequest -Uri $PS_START_SCRIPT -OutFile $save_path
-Write-Output "save as $save_path"
-
-Write-Output ""
-Write-Output ">> download nvim init.vim"
-$save_path = "$ENV:LOCALAPPDATA\nvim\init.vim"
-$url       = "$REPO_URL/nvim/init.vim"
-Invoke-WebRequest -Uri $url -OutFile $save_path
-Write-Output "save as $save_path"
-
-Write-Output ""
-Write-Output ">> download vim-plug"
-$save_path = "$ENV:LOCALAPPDATA\nvim-data\site\autoload\plug.vim"
-Invoke-WebRequest -Uri $PLUG_VIM -OutFile $save_path
-Write-Output "save as $save_path"
-
-Write-Output ""
-Write-Output ">> download vim template"
-$NVIM_TEMPLATE_FILES | ForEach-Object {
-
-    $save_path = "$ENV:LOCALAPPDATA\nvim\template\$PSItem"
-    $url       = "$REPO_URL/nvim/template/$PSItem"
-
-    Invoke-WebRequest -Uri $url -OutFile $save_path
-    Write-Output "save as $save_path"
-}
-
-Write-Output ""
-Write-Output ">> download my scripts"
-$MY_SCRIPTS | ForEach-Object {
-
-    $save_path = "$ENV:USERPROFILE\bin\$PSItem"
-    $url       = "$REPO_URL/bin/$PSItem"
-
-    Invoke-WebRequest -Uri $url -OutFile $save_path
-    Write-Output "save as $save_path"
-
-}
-
-Write-Output ""
-Write-Output ">> install scoop apps"
-Start-Process -FilePath powershell -ArgumentList $ENV:USERPROFILE\bin\scoop_update.ps1 -Wait
-
-Write-Output ""
-Write-Output ">> create desktop shortcut"
+Write-Output ">> Make desktop shortcut"
 $SHORTCUTS | ForEach-Object {
 
-	$path = "$ENV:USERPROFILE\Desktop\" + [System.IO.Path]::GetFileName($PSItem) + ".lnk"
+	$Dest = "$ENV:USERPROFILE\Desktop\" + [System.IO.Path]::GetFileName($PSItem) + ".lnk"
 
     $WSShell = New-Object -ComObject WScript.Shell
-    $ShortCut = $WSShell.CreateShortcut($path)
+    $ShortCut = $WSShell.CreateShortcut($Dest)
     $ShortCut.TargetPath   = $PSItem
     $ShortCut.IconLocation = $PSItem
     $ShortCut.Save()
 	
-    Write-Output "create shortcut as $path"
+    Write-Output "Shortcut as $Dest"
 }
 
 Write-Output ""
-Write-Output ">> install 3rd party modules"
+Write-Output ">> Install 3rd party modules"
 Install-Module -Name PSFzf -scope currentUser -Force
 Install-Module -Name ZLocation -scope currentUser -Force
 # PSFzfの読み込みとAlias有効化
@@ -124,7 +159,7 @@ Import-Module ZLocation
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
 
 Write-Output ""
-Write-Output ">> set git config"
+Write-Output ">> Set git config"
 git config --global core.autocrlf false
 git config --global core.editor nvim
 $res = (git config --global user.name)
@@ -135,18 +170,13 @@ git config --global core.autocrlf
 git config --global core.editor
 git config --global user.name
 
-Write-Output ""
-Write-Output ">> set pandoc templates"
-if (-Not(Test-Path "$ENV:APPDATA\pandoc")) {
-  New-Item "$ENV:APPDATA\pandoc" -ItemType Directory
-  New-Item "$ENV:APPDATA\pandoc\templates" -ItemType Directory
-}
 
-$END_DATE = Get-Date
-$elapced = ($END_DATE - $START_DATE).TotalSeconds 
-$last_message = "finished. (" + $elapced + "sec)"
+
+$EndDate = Get-Date
+$Elapced = ($EndDate - $StartDate).TotalSeconds 
+$LastMessage = "finished. (" + $Elapced + "sec)"
 
 
 Write-Output ""
-Write-Output $last_message
+Write-Output $LastMessage
 
